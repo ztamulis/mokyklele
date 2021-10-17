dc47a88124fb1e3bc6d251a2490a3753911f4850
Tamas <vagrant@homestead>
2021-10-17 11:06:20 +0000

    Forms update

--- resources/views/landing_other/question_form.blade.php
+++ resources/views/landing_other/question_form.blade.php
@@ -3,7 +3,7 @@
         @method("POST")
         @csrf
         <div class="form-group">
-            <label for="exampleFormControlInput1">Elektroninis paštas</label>
+            <label for="exampleFormControlInput1">Jūsų el.pašto adresas</label>
             <input type="email" class="form-control" name="email" id="exampleFormControlInput1" placeholder="name@example.com" required>
         </div>
         <div class="form-group">
@@ -14,19 +14,19 @@
                 <option value="Rišliai kalbu">Rišliai kalbu</option>
             </select>
         </div>
-        <div class="form-group">
-            <label for="exampleFormControlSelect2">Kokiomis dienomis norėtumėte, kad vyktų pamokos?</label>
-            <select required multiple class="form-control" name="week_days[]" id="exampleFormControlSelect2">
-                <option value="Pirmadieniais">Pirmadieniais</option>
-                <option value="Antradieniais">Antradieniais</option>
-                <option value="Trečiadieniais">Trečiadieniais</option>
-                <option value="Ketvirtadieniais">Ketvirtadieniais</option>
-                <option value="Penktadieniais">Penktadieniais</option>
-                <option value="Savaitgaliais">Savaitgaliais</option>
-            </select>
+        <div class="form-group" >
+            <fieldset>
+                <legend>Kokiomis dienomis norėtumėte, kad vyktų pamokos?</legend>
+                <input type="checkbox" name="week_days[]" id="track" value="Pirmadieniais" /><label for="track">Pirmadieniais</label><br />
+                <input type="checkbox" name="week_days[]" id="event" value="Antradieniais"  /><label for="event">Antradieniais</label><br />
+                <input type="checkbox" name="week_days[]" id="message" value="Trečiadieniais" /><label for="message">Trečiadieniais</label><br />
+                <input type="checkbox" name="week_days[]" id="message" value="Ketvirtadieniais" /><label for="message">Ketvirtadieniais</label><br />
+                <input type="checkbox" name="week_days[]" id="message" value="Penktadieniais" /><label for="message">Penktadieniais</label><br />
+                <input type="checkbox" name="week_days[]" id="message" value="Savaitgaliais" /><label for="message">Savaitgaliais</label><br />
+            </fieldset>
         </div>
-        <div class="form-group">
-            <label for="exampleFormControlSelect1">Kiek kartų per savaitę norėtumėte mokytis?</label>
+        <div class="form-group" style="margin-top: 15px;">
+            <label for="exampleFormControlSelect1" class="mt-3">Kiek kartų per savaitę norėtumėte mokytis?</label>
             <select required class="form-control" name="times_per_week" id="exampleFormControlSelect1" >
                 <option value="Vieną kartą">Vieną kartą</option>
                 <option value="Du kartus">Du kartus</option>
@@ -37,7 +37,7 @@
         <div class="form-group">
             <label for="exampleFormControlTextarea1">Papildomi komentarai/klausimai</label>
             <textarea class="form-control" name="comment" id="exampleFormControlTextarea1" rows="3"></textarea>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Siųsti</button>
         </div>
     </form>
 </div>
