9bfb856d1c13af141fc7a628a9d36bf4e0c6db24
vagrant <vagrant@homestead>
2021-11-26 13:33:29 +0000

    summernote font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -248,8 +248,12 @@
 <script>
     $(document).ready(function() {
         $('.summernote').summernote({
+            fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Helvetica', 'Impact', 'Tahoma', 'Times New Roman', 'Verdana', 'Roboto'],
+            fontNamesIgnoreCheck: ['Roboto'],
             height: 300
+
         });
+
     });
 </script>
 </body>
