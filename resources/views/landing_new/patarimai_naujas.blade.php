e148c244b04117ef72ebdf1cb05cfd3d96b7b5d4
vagrant <vagrant@homestead>
2021-12-10 12:53:41 +0000

    suagusiuju kursai update

--- resources/views/landing_new/patarimai_naujas.blade.php
+++ resources/views/landing_new/patarimai_naujas.blade.php
@@ -11,18 +11,16 @@
    <div class="row">
        @foreach ($suggestions as $suggestion)
            <div class="col-12 w-100 mt-5 suggestion-main">
-               <div class="suggestion-top mt-5 ml-5 mr-5">
+               <div class="suggestion-top mt-3 ml-5 mr-3">
                     <img class="img-fluid mt-2 mr-2 suggestion-img" src="{{asset('assets/img/other/Group 21.png')}}"> <h4 class="suggestion-title">{{$suggestion->title}}</h4>
                </div>
-              <div class="text mt-4 ml-5 mr-5 mb-5 suggestion-description" >{!! $suggestion->description !!}</div>
-
-               {{--               @if (strlen($suggestion->description) > 1000)--}}
-{{--                        <div class="text mt-4 ml-5 mr-5 mb-5 suggestion-description readmore" >{!! $suggestion->description !!}--}}
-{{--                        <span class="readmore-link"></span>--}}
-{{--                        </div>--}}
-{{--                @else--}}
-{{--                   <div class="text mt-4 ml-5 mr-5 mb-5 suggestion-description" >{!! $suggestion->description !!}</div>--}}
-{{--               @endif--}}
+               @if (strlen($suggestion->description) > 700)
+                        <div class="text mt-3 ml-5 mr-5 mb-3 suggestion-description readmore" >{!! $suggestion->description !!}
+                            <span class="readmore-link"></span>
+                        </div>
+                @else
+                   <div class="text mt-4 ml-5 mr-5 mb-3 suggestion-description" >{!! $suggestion->description !!}</div>
+               @endif
            </div>
        @endforeach
    </div>
