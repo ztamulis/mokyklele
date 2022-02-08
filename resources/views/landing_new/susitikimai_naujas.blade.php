ac35ed38277429d778dcd544fcd966490ff6c666
Tamulis <vagrant@homestead>
2022-02-08 12:01:39 +0000

    meetings reordered

--- resources/views/landing_new/susitikimai_naujas.blade.php
+++ resources/views/landing_new/susitikimai_naujas.blade.php
@@ -14,9 +14,9 @@
     <div class="row">
         <div class="col-md-12">
             <div class="tabs-meetings">
-                <button class="tablinks-meetings active" data-country="tab-1">Visi susitikimai</button>
+                <button class="tablinks-meetings active" data-country="tab-1">Susitikimai kurse</button>
                 <button class="tablinks-meetings" data-country="tab-2">Praėję susitikimai</button>
-                <button class="tablinks-meetings" data-country="tab-3">Susitikimai kurse</button>
+                <button class="tablinks-meetings" data-country="tab-3">Visi susitikimai</button>
             </div>
         </div>
     </div>
@@ -25,11 +25,11 @@
         <div class="wrapper_tabcontent">
             <div id="tab-1" class="tabcontent-meetings active">
                 <div class="row">
-                    @foreach ($meetings as $meeting)
+                    @foreach ($coming as $meeting)
                         <div class="col-md-6 col-lg-4">
                             @if($meeting->photo)
                                 <div>
-                                <img class="img-fluid meeting-img" src="/uploads/introductions/{{ $meeting->photo }}">
+                                    <img class="img-fluid meeting-img" src="/uploads/introductions/{{ $meeting->photo }}">
                                 </div>
                             @endif
                             <div class="meeting-content-align">
@@ -40,7 +40,7 @@
                                     {{App\Models\Group::getWeekDay($meeting->date_at->timezone(Cookie::get("user_timezone", "GMT"))->dayOfWeek)}},<br>
                                     <span><b>{{ App\TimeZoneUtils::updateTime($meeting->date_at->timezone(Cookie::get("user_timezone", "GMT"))->format("H:i"), $meeting->updated_at) }}</b></span>
                                     ({{Cookie::get("user_timezone", "GMT")}})</div>
-                                <div class="meeting-description mt-3">{!! $meeting->description !!}</div>
+                                <div class="meeting-description mt-3 mb-3">{!! $meeting->description !!}</div>
                             </div>
                         </div>
                     @endforeach
@@ -71,7 +71,7 @@
             </div>
             <div id="tab-3" class="tabcontent-meetings">
                 <div class="row">
-                    @foreach ($coming as $meeting)
+                    @foreach ($meetings as $meeting)
                         <div class="col-md-6 col-lg-4">
                             @if($meeting->photo)
                                 <div>
