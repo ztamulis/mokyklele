b6f5f0d0b5e17bf4dc7f00c2d01ed73032861352
ztamulis <zygintas.tamulis@gmail.com>
2022-02-14 18:12:55 +0200

    meetings filter reworked

--- resources/views/dashboard/error.blade.php
+++ resources/views/dashboard/error.blade.php
@@ -1,4 +1,6 @@
-<x-app-layout>
+<x-user>
+    <div class="container--other">
+
     @if(isset($error))
         <div class="row">
             <div class="col-xl-8 offset-xl-2">
@@ -6,4 +8,5 @@
             </div>
         </div>
     @endif
-</x-app-layout>
+    </div>
+</x-user>
