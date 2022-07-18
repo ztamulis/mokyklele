ecf9a76bfcb009debd3d05cb4573cb39463a7e01
ztamulis <zygintas.tamulis@gmail.com>
2022-07-18 14:42:32 +0300

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,11 +5,17 @@
 
     <link rel="stylesheet" type="text/css" href="/css/landing.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MM886HG');</script>
+    <!-- End Google Tag Manager -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
 <body>
-
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MM886HG"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <div class="wrapper">
 
     <header>
