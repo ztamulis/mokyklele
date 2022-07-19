d25c881a2de49c6f3f286fe5d766c7bb179a8f13
ztamulis <zygintas.tamulis@gmail.com>
2022-07-19 14:13:47 +0300

    Update group_order_succeeded.blade.php

--- resources/views/lessons_order/group_order_succeeded.blade.php
+++ resources/views/lessons_order/group_order_succeeded.blade.php
@@ -59,10 +59,10 @@
                             {
                                 'name': '{{$group->name}}',   // Replace XXX with a name of a class (example: Antradieniais (1 lygis))
                                 'id': '{{$group->id}}',   // Replace XXX with ID of selected class
-                                'category': '{{$group->type}}',   // Please replace XXX with category of selected class (Should be either 'Mokama' or 'Nemokama')
-                                'quantity': '{{count($payment->students)}}',   // Please replace XXX with a quantity of hours of a selected class (only numbers are allowed. For example, if there is a text '2 pamokos', insert only number 2)
+                                'category': '{{$group->paid ? 'Mokama' : 'Nemokama'}}',   // Please replace XXX with category of selected class (Should be either 'Mokama' or 'Nemokama')
+                                'quantity': '{{isset($group->getGroupStartDateAndCount()['eventsCount']) ? $group->getGroupStartDateAndCount()['eventsCount'] : '0'}}',   // Please replace XXX with a quantity of hours of a selected class (only numbers are allowed. For example, if there is a text '2 pamokos', insert only number 2)
                                 'price': '{{$group->price}}',   // Replace XXX with price of a selected class (example: 111.00 (it is mandatory to use a dot in the price and .00 if neccessary))
-                                'level': '{{ $group->display_name }}',   // Replace XXX with a level of a group in which class is (examples: Mėlyna (7-9m.), Raudona (10-14m.))
+                                'level': '{{$group::getGroupTypeTranslated($group->type)}}',   // Replace XXX with a level of a group in which class is (examples: Mėlyna (7-9m.), Raudona (10-14m.))
                                 'hour': '{{ App\TimeZoneUtils::updateTime($group->time->timezone("Europe/London"), $group->updated_at)->format('H:i')}}',   // Replace XXX with a hour of a class (examples: 09:00, 19:00)
                                 'description': '{{ $group->description }}',   // Replace XXX with a description of a class (example: Pamokos 7-9 m. vaikams)
                                 'dates': '{{isset($group->startDate) ? \Carbon\Carbon::parse($group->startDate)->format("m.d") : '0'}} - {{\Carbon\Carbon::parse($group->end_date)->format("m.d")}}'   // Replace XXX with a dates of a class (example: 07.12 - 07.12)
